04cf40e5951d7d95bbf0f6f60924595968b6c412
Manish Mittal <manish.mittal57@gmail.com>
2019-11-01 01:24:12 +0530

    push mock data

---
+++ HRIS-Angular-UI/src/app/mock-data/mock-data.ts
@@ -0,0 +1,29 @@
+export const PRIMARY_KEY: any[] =
+[
+  {
+    name: 'Earl of Lemongrab',
+    age: 'Unknown',
+    species: 'Lemon Candy',
+    occupation: 'Earl, Heir to the Candy Kingdom Throne'
+  },
+  {
+    name: 'Bonnibel Bubblegum',
+    age: '19',
+    species: 'Gum Person',
+    occupation: 'Returned Ruler of the Candy Kingdom'
+  },
+  {
+    name: 'Phoebe',
+    age: '16',
+    species: 'Flame Person',
+    occupation: 'Ruler of the Fire Kingdom'
+  },
+  {
+    name: 'Lumpy Space Princess',
+    age: '18',
+    species: 'Lumpy Space Person', 
+    occupation: 'Babysitter'
+  },
+]
+
+export const QUARTER: any[] = ['Q4-2019','Q3-2019','Q2-2019','Q1-2019','Q4-2018','Q3-2018','Q2-2018','Q1-2018'];
