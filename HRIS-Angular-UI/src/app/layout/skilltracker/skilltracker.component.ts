b1eec7057d9bf4daec780aea976a6ead1677fe57
Manish Mittal <manish.mittal57@gmail.com>
2019-11-08 09:28:11 +0530

    updated code

--- HRIS-Angular-UI/src/app/layout/skilltracker/skilltracker.component.ts
+++ HRIS-Angular-UI/src/app/layout/skilltracker/skilltracker.component.ts
@@ -22,7 +22,7 @@ export class SkilltrackerComponent implements OnInit {
     isPrimarySkills: boolean = true;
     isHorizonSkills:boolean = true;
     isDataAddedinHorizon:boolean = false;
-    logitivityScore:Array<any> = [];
+    longivityScore:Array<any> = [];
     experienceScore:Array<any> = [];
     Evaluated:Array<any> = [];
     display='none';
@@ -34,16 +34,28 @@ export class SkilltrackerComponent implements OnInit {
     managerColumns: string[];
     EvaluatedData:Array<any> = [];
     skillsAddedfromPopup:Array<any> = [];
-
+    matrixScore:Array<any> = [];
+    skillMaster:Array<any>=[0,0,0];
+    skillResult=0;
+    skillModels:Array<any>=[];
     constructor(
         private http: Http, private globals : Globals, private STService : SkilltrackerService
     ) {
+        this.matrixScore=[
+            {'id':0,'name':'0 - Not Used'},
+            {'id':1,'name':'1 - Basic'},
+            {'id':2,'name':'2 - Intermediate1'},
+            {'id':3,'name':'3 - Intermediate2'},
+            {'id':4,'name':'4 - Expert'}
+        ];
         STService.getPrimarySkills().subscribe((data : any[])=>{
             if(data.length > 0){
                 for(let i=0;i<data.length;i++){
                 let dt = data[i].s_name;
                     dt.replace(' ','_');
                     data[i].s_model = dt;
+                    data[i].isEvaluated = "No";
+                    data[i].evaluatedDate = "05-11-2019";
                     if(data[i].s_type.toLowerCase() == 'primary'){
                         this.skillDetails.push(data[i]);
                     } else {
@@ -53,6 +65,16 @@ export class SkilltrackerComponent implements OnInit {
             }
         }, err => {
             console.log(err);
+            /*var data = [{"s_id":1,"s_name":"Java","s_type":"primary","timestamp":"2019-11-03T18:14:35.000Z","deleted_ts":"2019-11-03T18:14:35.000Z"},{"s_id":2,"s_name":"PHP","s_type":"horizon3","timestamp":"2019-11-03T18:14:50.000Z","deleted_ts":"2019-11-03T18:14:50.000Z"},{"s_id":3,"s_name":"Angular","s_type":"horizon3","timestamp":"2019-11-04T17:05:54.000Z","deleted_ts":"2019-11-04T17:05:54.000Z"},{"s_id":4,"s_name":"NodeJs","s_type":"primary","timestamp":"2019-11-04T17:05:54.000Z","deleted_ts":"2019-11-04T17:05:54.000Z"}];
+            if(data.length > 0){
+                for(let i=0;i<data.length;i++){
+                    if(data[i].s_type.toLowerCase() == 'primary'){
+                        this.skillDetails.push(data[i]);
+                    } else {
+                        this.secondarySkillDetails.push(data[i]);
+                    }
+                }
+            }*/
         })
 
     }
@@ -62,7 +84,7 @@ export class SkilltrackerComponent implements OnInit {
         this.columns = this.STService.getColumns();
         //this.skillDetails = this.STService.getPrimarySkills();
         //console.log(this.skillDetails);
-        this.logitivityScore = this.STService.getLogitivityScore();
+        this.longivityScore = this.STService.getLongivityScore();
         this.experienceScore = this.STService.getExperienceScore();
         this.Evaluated = this.STService.getEvaluatedDetails(); 
         this.employeeId = this.STService.getEmployeeDetails();
@@ -82,11 +104,50 @@ export class SkilltrackerComponent implements OnInit {
     
       }
 
-    filterLogitivityScore(param) {
+    calculateSkillScore(index,param,_inx) {
+        this.skillMaster[index]=param;
+        console.log(this.skillDetails);
+        if(index == 0){
+            this.skillDetails[_inx].matrixScore = param;
+        } else if(index == 1){
+            this.skillDetails[_inx].longivityScore = param;
+        } else if(index == 2){
+            this.skillDetails[_inx].experienceScore = param;
+        }
 
+        var sumNumber = this.skillMaster.reduce((acc, cur) => acc + Number(cur), 0);
+        this.skillResult=sumNumber/3;
+        
     }
 
     onSubmit() {
+        var jsonData = []
+        for(var i=0;i<this.skillDetails.length;i++){
+            var obj = {
+                "e_id" : 0,
+                "s_id" : this.skillDetails[i].s_id,
+                "s_type": "1",
+                "matrix_score" : this.skillDetails[i].matrixScore,
+                "longivity_score" : this.skillDetails[i].longivityScore,
+                "experience_score" : this.skillDetails[i].experienceScore,
+                "evaluated" : "0",
+                "manager_e_id" : 0
+            };
+            jsonData.push(obj);
+        }
+        let fd = new FormData();
+        let passDetails = JSON.stringify(jsonData);
+        fd.append('jsonData',passDetails);
+
+        this.STService.saveSkillInformation(fd).subscribe(data=>{
+            let res:any = data;
+            if(res.status == "success"){
+              alert("success")
+            }else{
+              alert("error")
+            }
+          });
+        
 
     }
 
--- HRIS-Angular-UI/src/app/provider/skilltracker/skilltracker.service.ts
+++ HRIS-Angular-UI/src/app/provider/skilltracker/skilltracker.service.ts
@@ -3,7 +3,8 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/delay';
 import { PRIMARY_KEY } from '../../mock-data/mock-data';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Http, Response, Headers, RequestOptions} from "@angular/http";
 
 @Injectable()
 export class SkilltrackerService {
@@ -16,7 +17,7 @@ export class SkilltrackerService {
   }
 
   getColumns(): string[]{
-    return ["Sl No", "Skills", "Matrix Score", "Logitivity Score", "Experience Score", "Skill Score", "Evaluated", "Evaluated on"];
+    return ["Sl No", "Skills", "Matrix Score", "Longivity Score", "Experience Score", "Skill Score", "Evaluated", "Evaluated on"];
   }
 
   getQuarter(): string[]{
@@ -28,12 +29,12 @@ export class SkilltrackerService {
     return this.http.get(this.service_url+'getAllSkillType');
   }
 
-  getLogitivityScore(): Array<any> {
-    return [{id:1, name:"1.0"},{id:2, name:"2.0"},{id:3, name:"3.0"},{id:4, name:"4.0"}];
+  getLongivityScore(): Array<any> {
+    return [{id:1, name:"1 - [More Than 18 Months]"},{id:2, name:"2 - [8-18 Months]"},{id:3, name:"3 - [4-7 Months]"},{id:4, name:"4 - [0-3 Months]"}];
   }
 
   getExperienceScore(): Array<any> {
-    return [{id:1, name:1},{id:2, name:2},{id:3, name:3},{id:4, name:4}];
+    return [{id:1, name:"1 - [0-3 Months]"},{id:2, name:"2 - [4-7 Months]"},{id:3, name:"3 - [8-15 Months]"},{id:4, name:"4 - [More Than 18 Months]"}];
   }
 
   getEmployeeDetails(): Array<any> {
@@ -51,5 +52,21 @@ export class SkilltrackerService {
   geEvaluatedData(): Array<any> {
     return [{id:2019001, name:'Saismita'},{id:2019002, name:'Debashri'},{id:2009003, name:'Avinandan'}];
   }
+
+  saveSkillInformation(data){
+
+  let headers = new Headers({ 'Content-Type': 'application/json' });
+    //let options = new RequestOptions({ headers: headers });
+    //let body = data;//JSON.stringify(food);
+    //return this.http.post(this.service_url+"addSkillTracker", body, options ).map((res: Response) => res.json());
+
+    //const headers = new Headers;
+    //let headers =  {headers: new  HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded'})};
+    //headers.append('Content-Type', 'application/json');
+    return this.http.post(this.service_url+"addSkillTracker", data)
+    //.pipe(map((res => res)));
+    //return this.http.post(this.service_url+"addSkillTracker",data, options);
+  }
+  
 
 }
